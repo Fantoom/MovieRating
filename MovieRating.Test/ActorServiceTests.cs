8c27bdc1923db901d762958b17dc6ccdfce67f50
Phantom <P_hanto_m@outlook.com>
2022-08-31 18:57:52 +0400

    Add ActorServiceTests

---
+++ MovieRating.Test/ActorServiceTests.cs
@@ -0,0 +1,153 @@
+using MovieRating.Data;
+using MovieRating.Data.Models;
+using MovieRating.Services;
+using X.PagedList;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MovieRating.Data.Models.Dtos;
+
+namespace MovieRating.Test
+{
+    public class ActorServiceTests : IDisposable
+    {
+
+        private ApplicationDbContext context;
+        private ActorService actorService;
+        private List<Actor> generatedActors;
+        private static readonly string testUserId = "TestUserId";
+
+        public ActorServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                              .UseInMemoryDatabase("ActorTestDB")
+                              .Options;
+            context = new ApplicationDbContext(options);
+            context.Database.EnsureDeleted();
+
+            generatedActors = GetTestData();
+            context.Actors.AddRange(generatedActors);
+            context.SaveChanges();
+            actorService = new ActorService(context);
+        }
+
+        [Theory]
+        [InlineData(5, null)]
+        [InlineData(5, "TestUserId")]
+
+        public async Task GetTopActorsAsyncTest(int count, string? userId = null)
+        {
+            var serviceActors = await actorService.GetTopActorsAsync(count, userId) as IEnumerable<ActorWithRatingDto>;
+            var expectedActors = generatedActors.SelectAllActorsWithRatings(userId)
+                                            .OrderByDescending(x => x.AverageRating)
+                                            .Take(count);
+            Assert.Equal(expectedActors, serviceActors);
+        }
+
+        [Theory]
+        [InlineData(1, null)]
+        [InlineData(1, "TestUserId")]
+        public async Task GetActorWithRatingAndMoviesAsyncTest(int actorId, string? userId = null)
+        {
+            var serviceActor = await actorService.GetActorWithRatingAndMoviesAsync(actorId, userId);
+            var expectedActor = generatedActors.SelectAllActorsWithRatingsAndMovies(userId)
+                                           .First(x => x.Id == actorId);
+
+            Assert.Equal(expectedActor.UserRating, serviceActor.UserRating);
+        }
+
+        [Theory]
+        [InlineData("TestUserId", 1)]
+        public async Task GetUserActorRatingAsyncTest(string userId, int actorId)
+        {
+            var serviceActorRating = await actorService.GetUserActorRatingAsync(userId, actorId);
+            var expectedRating = generatedActors.First(x => x.Id == actorId).Ratings
+                                            .First(x => x.UserId == userId);
+
+            Assert.Equal(expectedRating, serviceActorRating);
+        }
+
+        [Theory]
+        [InlineData(1, 5, "TestUserId")]
+        [InlineData(2, 5, "TestUserId")]
+        public async Task GetPagedActorsWithRatingsAsyncTest(int page, int pageSize, string userId)
+        {
+            var serviceActors = await actorService.GetPagedActorsWithRatingsAsync(page, pageSize, userId) as IEnumerable<ActorRatingDto>;
+            var expectedActors = generatedActors.SelectAllActorsWithRatings(userId).ToPagedList(page, pageSize) as IEnumerable<ActorRatingDto>;
+
+            Assert.Equal(expectedActors, serviceActors);
+        }
+
+        private static List<Actor> GetTestData()
+        {
+            var movies = Enumerable.Range(1, 10).Select(movieId => new Movie()
+            {
+                Id = movieId,
+                Title = $"Movie{movieId}",
+                Description = $"MovieDescription{movieId}",
+                ReleaseDate = DateTime.Parse("30.08.2022"),
+                Ratings = Enumerable.Range(1, 5).Select(ratingId => new MovieRatingModel
+                {
+                    Id = movieId * 1000 + ratingId,
+                    MovieId = movieId,
+                    Rating = ratingId,
+                    UserId = testUserId
+                }).ToList()
+            }).ToList();
+
+            var actors = Enumerable.Range(1, 10).Select(actorId => new Actor()
+            {
+                Id = actorId,
+                Name = $"Actor{actorId}",
+                Movies = movies.Take(actorId).ToList(),
+                Ratings = Enumerable.Range(1, 5).Select(ratingId => new ActorRating
+                {
+                    Id = actorId * 1000 + ratingId,
+                    ActorId = actorId,
+                    Rating = ratingId,
+                    UserId = testUserId
+                }).ToList()
+            }).ToList();
+
+            return actors;
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+    }
+
+    internal static class ActorTestExt
+    {
+        internal static IEnumerable<ActorWithRatingDto> SelectAllActorsWithRatings(this IEnumerable<Actor> movies, string? userId = null)
+        {
+            return movies.Select(x => new ActorWithRatingDto()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                AverageRating = x.Ratings.Average(r => r.Rating),
+                UserRating = x.Ratings.FirstOrDefault(r => r.UserId == userId)?.Rating ?? null
+            });
+        }
+        internal static IEnumerable<ActorWithRatingAndMoviesDto> SelectAllActorsWithRatingsAndMovies(this IEnumerable<Actor> movies, string? userId = null)
+        {
+            return movies.Select(x => new ActorWithRatingAndMoviesDto()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                AverageRating = x.Ratings.Average(r => r.Rating),
+                UserRating = x.Ratings.FirstOrDefault(r => r.UserId == userId)?.Rating,
+                Movies = x.Movies.Select(m => new MovieWithRatingDto()
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Description = m.Description,
+                    ReleaseDate = m.ReleaseDate,
+                    AverageRating = m.Ratings.Average(r => r.Rating),
+                    UserRating = m.Ratings.FirstOrDefault(r => r.UserId == userId)?.Rating ?? null
+                }).ToList()
+            });
+        }
+    }
+}
