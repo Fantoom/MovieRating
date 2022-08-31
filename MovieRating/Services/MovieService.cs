d41620601b052f29a893eb6fa52c7b2ded3d3799
Phantom <P_hanto_m@outlook.com>
2022-08-31 16:46:07 +0400

    Remove useless using

--- MovieRating/Services/MovieService.cs
+++ MovieRating/Services/MovieService.cs
@@ -1,5 +1,4 @@
-﻿using Mapster;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MovieRating.Data;
 using MovieRating.Data.Models;
 using MovieRating.Data.Models.Dtos;
