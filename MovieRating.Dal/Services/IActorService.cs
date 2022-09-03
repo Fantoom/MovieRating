33445d95cc8c7b71a0f3d3f1b5b7491768cf001c
Phantom <P_hanto_m@outlook.com>
2022-09-03 23:35:13 +0400

    add default param values in IActorService

--- MovieRating.Dal/Services/IActorService.cs
+++ MovieRating.Dal/Services/IActorService.cs
@@ -7,8 +7,8 @@ namespace MovieRating.Dal.Services
     {
         Task AddRatingAsync(string userId, int actorId, int rating);
         Task<ActorWithRatingAndMoviesDto> GetActorWithRatingAndMoviesAsync(int actorId, string? userId = null);
-        Task<AsyncPagedList<ActorWithRatingDto>> GetPagedActorsWithRatingsAsync(int pageNumber, int pageSize, string? userId);
-        Task<List<ActorWithRatingDto>> GetTopActorsAsync(int count, string? userId);
+        Task<AsyncPagedList<ActorWithRatingDto>> GetPagedActorsWithRatingsAsync(int pageNumber, int pageSize, string? userId = null);
+        Task<List<ActorWithRatingDto>> GetTopActorsAsync(int count, string? userId = null);
         Task<ActorRatingDto?> GetUserActorRatingAsync(string userId, int actorId);
     }
 }
