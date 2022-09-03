46528c6398b74e3717959a77cf4afe880cd7ac1e
Phantom <P_hanto_m@outlook.com>
2022-09-03 23:24:20 +0400

    Move AsyncPageListExt into separate file

---
+++ MovieRating.Dal/Data/AsyncPageListExt.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieRating.Dal.Data
+{
+    public static class AsyncPageListExt
+    {
+        public static Task<AsyncPagedList<T>> ToAsyncPagedList<T>(this IQueryable<T> superset, int pageNumber, int pageSize)
+        {
+            return AsyncPagedList<T>.CreateAsync(superset, pageNumber, pageSize);
+        }
+    }
+}
--- MovieRating.Dal/Data/AsyncPagedList.cs
+++ MovieRating.Dal/Data/AsyncPagedList.cs
@@ -39,12 +39,4 @@ namespace MovieRating.Dal.Data
             return pagedList;
         }
     }
-
-    public static class AsyncPageListExt
-    {
-        public static Task<AsyncPagedList<T>> ToAsyncPagedList<T>(this IQueryable<T> superset, int pageNumber, int pageSize)
-        {
-            return AsyncPagedList<T>.CreateAsync(superset, pageNumber, pageSize);
-        }
-    }
 }
