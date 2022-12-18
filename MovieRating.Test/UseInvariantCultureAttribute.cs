641c03a2471a320a1038ba4f77b820baf7451e32
Phantom <P_hanto_m@outlook.com>
2022-12-18 19:09:06 +0400

    Add UseInvariantCultureAttribute

---
+++ MovieRating.Test/UseInvariantCultureAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Reflection;
+using Xunit.Sdk;
+
+
+namespace MovieRating.Test;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class UseInvariantCultureAttribute : BeforeAfterTestAttribute
+{
+    CultureInfo originalCulture = null!;
+    CultureInfo originalUICulture = null!;
+
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        originalCulture = Thread.CurrentThread.CurrentCulture;
+        originalUICulture = Thread.CurrentThread.CurrentUICulture;
+
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+        CultureInfo.CurrentCulture.ClearCachedData();
+        CultureInfo.CurrentUICulture.ClearCachedData();
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        Thread.CurrentThread.CurrentCulture = originalCulture;
+        Thread.CurrentThread.CurrentUICulture = originalUICulture;
+
+        CultureInfo.CurrentCulture.ClearCachedData();
+        CultureInfo.CurrentUICulture.ClearCachedData();
+    }
+}
